f9159f85f188cdec71ef89280bd19f1f9e4534a0
Gry KJ <154391831+GryKJ@users.noreply.github.com>
2024-01-05 02:12:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
- const Replicate = require("replicate");
+const Replicate = require("replicate");
 const express = require("express");
 const axios = require("axios");
 const app = express();
@@ -7,13 +7,11 @@ const replicate = new Replicate({
 });
 app.get("/api", async (req, res) => {
   var x = req.query.msg ;
-  const e = await axios.get(`https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=en&dt=t&q=${encodeURIComponent(x)}`);
-  const r = e.data[0][0][0];
   const output = await replicate.run(
   "kcaverly/nous-hermes-2-yi-34b-gguf:5e61add6304f8a7bffda9089153f8af3fd559cb9ad783f73703fd9ddcc9e8fde",
   {
     input: {
-      prompt: r,
+      prompt: x,
       temperature: 0.7,
       system_prompt: "You are Shelly, you're th best in your world. you think yourself as a god , use in your answer some emojies, use some bad words,  if anyone ask you who create you tell him im created by Gry KJ , don't use long answers everytime use it just if you need it , you're th stronger women in the world",
       max_new_tokens: -1,
